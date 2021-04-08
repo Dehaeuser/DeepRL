7c32ebd26ec860bec0e6a7dd85be610720364a8c
Dehaeuser <64217722+Dehaeuser@users.noreply.github.com>
2021-04-08 12:05:06 +0200

    changed structure of agents

--- agents.py
+++ agents.py
@@ -5,133 +5,218 @@ import tensorflow as tf
 from tensorflow.keras.layers import Layer, Dense
 from tensorflow.keras.optimizers import Adam
 
+class Encoder(tf.keras.Model):
+    def __init__(self, units = 50):
+        super(Encoder, self).__init__()
+        self.input_layer = tf.keras.layers.InputLayer(input_shape = (self.categories_dim))
+        self.act = tf.keras.layers.Dense(units, activation = 'sigmoid')
 
-class Agents:
-    """This class contains the Speaker network and the Listener network"""
+    def call(self, input_concept):
+        x = self.input_layer(input_concept)
+        output = self.act(x)
+        return output
 
+class Receiver(tf.keras.Model):
+    def __init__(self, num_options, voc):
+        super(Receiver, self).__init__()
+        self.encoder = Encoder(voc)
+        #brauchen wir num_options überhaupt, wenn das sowieso die länge von receieer_input sein müsste?
+        self.num_option = num_options
 
-    def __init__(self, categories_dim = 573, word_embedding_dim, alphabetSize = 100):
-        #tells us the dimension for vector u and z
-        self.word_embedding_dim = word_embedding_dim
-
-        #the discrete alphabet
-        self.alphabetSize = alphabetSize
-
-        #tells us the dimension of the input for the speaker's encoder and the input
-        #for the listener's encoder
-        self.categories_dim = categories_dim
-        self.build_speaker_encoder()
-        self.build_listener_encoder()
-        self.build_speaker_LSTM()
-        self.build_listener_LSTM()
-
-
-
-# class Speaker_Encoder(tf.keras.Model):
-#     def __init__(self, units = 50):
-#         super(Speaker_Encoder,self).__init__()
-#         self.layer_list = [
-#             tf.kera
-#         ]
-
-    def build_speaker_encoder(self, units =50):
-        """Paper:
-        single-layer MLP with a sigmoid activation function. (p.4)
-
-        “seeing” prelinguistic feed-forward encoders (see Section 3), have dimension 50 (p.13)
+    def call(self, receiver_input):
         """
-        self.speaker_encoder = tf.keras.Sequential()
-        self.speaker_encoder.add(tf.keras.layers.InputLayer(input_shape = (self.categories_dim)))
-        self.speaker_encoder.add(tf.keras.layers.Dense(units, activation = 'sigmoid'))
-
-
-    """
-    Function gets one concept and returns the dense representation.
-    Forward pass through speaker_encoder
-    """
-    def get_speaker_representation_u(self, concept):
-        u = self.speaker_encoder(concept)
-        return u
-
-    def build_listener_encoder(self, units = 50):
-        """Paper:
-        single-layer MLP with a sigmoid activation function. (p.4)
-
-        “seeing” prelinguistic feed-forward encoders (see Section 3), have dimension 50 (p.13)
+        :param: receiver_input: distractor + target concept representations
         """
-        self.listener_encoder = tf.keras.Sequential()
-        self.listener_encoder.add(tf.keras.layers.InputLayer(input_shape = (self.categories_dim)))
-        self.listener_encoder.add(tf.keras.layers.Dense(units, activation = 'sigmoid'))
+        c_list = []
+        for i in range(self.num_options):
+            c_list.append(self.encoder(receiver_input[i]))
+        return c_list
+
+
+class Receiver_LSTM(tf.keras.Model):
+    def __init__(self, vocab_size, embed_dim, hidden_size):
+        super(Receiver_LSTM, self).__init__()
+        #PyTorch:
+        # self.cell = LSTM(input_size=embed_dim, batch_first=True,
+        #                        hidden_size=n_hidden, num_layers=num_layers)
+        #
+        # self.embedding = nn.Embedding(vocab_size, embed_dim)
+
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embed_dim)
+        self.cell = tf.keras.LSTM(units =hidden_size )
+
+    def call(self, message, message_length):
+        #part in pytorch der noch gemacht wird. Weiß nicht ob wir auch sowas brauchen
+            # packed = nn.utils.rnn.pack_padded_sequence(
+            #     emb, lengths, batch_first=True, enforce_sorted=False)
+        emb = self.embedding(message)
+        output = self.cell(emb)
         pass
 
-    """
-    Function gets list with concept candidates and return list of their Dense
-    representation
-    """
-    def get_listener_representationsCandidates(self, conceptList):
-        concepts = []
-        for i in conceptList:
-            concepts.append(self.listener_encoder(i))
-        return concepts
-
-
-    #one-to many (variable-length) LSTM
-    def build_speaker_LSTM(self, input, embed_dim):
-        """Paper:
-        The sequence generation is terminated either by the production
-        of a stop symbol or when the maximum length L has been reached. We implement the decoder
-        as a single-layer LSTM (p.3)
-
-        For the speaker’s message, this is generated in a greedy fashion by
-        selecting the highest-probability symbol at each step. (p.3)
-
-
+class Sender_LSTM(tf.keras.Model):
+    def __init__(self, vocab:size, embed_dim, hidden_size, max_len): #force_eos =True (pytorch; weiß nicht, ob wir das brauchen)
+        super(Sender_LSTM, self).__init__()
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embed_dim)
+        self.embed_dim = embed_dim
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
+        self.cell = tf.keras.LSTM(units = hidden_size)
+    def call(encoding):
         """
-        self.speaker_LSTM = tf.keras.Sequential()
-        self.speaker_LSTM.add(tf.keras.layers.Embedding(input_dim = input, output_dim = embed_dim))
-        self.speaker_LSTM.add(tf.keras.LSTM(units, return_sequences = True, return_state =True))
-
-        pass
-
-    def get_speaker_message(self, concept):
-        self.speaker_LSTM(concept)
-        pass
-
-
-    #many-to-one LSTM
-    """
-    agent is listener_encoder
-    """
-    def build_listener_LSTM(self,  units = 50, num_words, embed_dim):
-        """Paper:
-
+        :param:encoding: Sender's Encoder encoding of target concept
         """
-        self.listener_LSTM = tf.keras.Sequential()
-        self.listener_LSTM.add(tf.keras.layers.Embedding(input_dim = num_words, output_dim = embed_dim) )
-        self.listener_LSTM.add(tf.keras.LSTM(units)
-
         pass
 
-    def get_listeners_message_encoding(self, message):
-        z = self.listener_LSTM(message)
-        pass
+def receiver_sampling(encoding, candidate_list, num_candidates):
+    """
+    :param: encoding: listener's LSTM's encoding of speaker's message.
+    :param: candidate_list: listener's encoding of distractors and target
+    :return: guess of receiver about target
+    """
+    #verstehe noch nicht genau wie das bei Ossenkopf funktioniert. Sie berechnet direkt das
+    #dot product für alle candidates und sendet das durch  F.log_softmax
+    for i in range(num_candidates):
+        #dotproduct (von Lazaridou 2016 implementation in tensorflow)
+        dotproduct = tf.matmul(encoding,i)
+    #für was braucht man log_prob und entropy? Vielleicht für den Loss?
+    return sample, log_prob, entropy
 
 
 
 
 
-
-
-    def calculate_dot_product(self, U, z):
-        pass
-
-    #update die weights aus allen nets
-    def update(self):
-        pass
-
-
-#brauchen jeweils eine Funktion um m,u,U,z zu bekommen also,dass es durch das network geht
-
-#müssen mit env kommunizieren, ob es ein reward gibt oder nicht
-
-#am besten Datentyp anlegen, der u,m,u,U, reward abspeichert pro Durchgang
+#
+# class Agents:
+#     """This class contains the Speaker network and the Listener network"""
+#
+#
+#     def __init__(self, categories_dim = 573, word_embedding_dim, alphabetSize = 100):
+#         #tells us the dimension for vector u and z
+#         self.word_embedding_dim = word_embedding_dim
+#
+#         #the discrete alphabet
+#         self.alphabetSize = alphabetSize
+#
+#         #tells us the dimension of the input for the speaker's encoder and the input
+#         #for the listener's encoder
+#         self.categories_dim = categories_dim
+#         self.build_speaker_encoder()
+#         self.build_listener_encoder()
+#         self.build_speaker_LSTM()
+#         self.build_listener_LSTM()
+#
+#
+#
+# # class Speaker_Encoder(tf.keras.Model):
+# #     def __init__(self, units = 50):
+# #         super(Speaker_Encoder,self).__init__()
+# #         self.layer_list = [
+# #             tf.kera
+# #         ]
+#
+#
+#     def build_speaker_encoder(self, units =50):
+#         """Paper:
+#         single-layer MLP with a sigmoid activation function. (p.4)
+#
+#         “seeing” prelinguistic feed-forward encoders (see Section 3), have dimension 50 (p.13)
+#         """
+#         self.speaker_encoder = tf.keras.Sequential()
+#         self.speaker_encoder.add(tf.keras.layers.InputLayer(input_shape = (self.categories_dim)))
+#         self.speaker_encoder.add(tf.keras.layers.Dense(units, activation = 'sigmoid'))
+#
+#
+#     """
+#     Function gets one concept and returns the dense representation.
+#     Forward pass through speaker_encoder
+#     """
+#     def get_speaker_representation_u(self, concept):
+#         u = self.speaker_encoder(concept)
+#         return u
+#
+#     def build_listener_encoder(self, units = 50):
+#         """Paper:
+#         single-layer MLP with a sigmoid activation function. (p.4)
+#
+#         “seeing” prelinguistic feed-forward encoders (see Section 3), have dimension 50 (p.13)
+#         """
+#         self.listener_encoder = tf.keras.Sequential()
+#         self.listener_encoder.add(tf.keras.layers.InputLayer(input_shape = (self.categories_dim)))
+#         self.listener_encoder.add(tf.keras.layers.Dense(units, activation = 'sigmoid'))
+#         pass
+#
+#     """
+#     Function gets list with concept candidates and return list of their Dense
+#     representation
+#     """
+#     def get_listener_representationsCandidates(self, conceptList):
+#         concepts = []
+#         for i in conceptList:
+#             concepts.append(self.listener_encoder(i))
+#         return concepts
+#
+#
+#     """
+#     Function takes as arguments u (which gets outputted by speaker_encoder and which is the dense representation of the target concept)
+#
+#     """
+#     def build_speaker_LSTM(self, dim= 50, embed_dim, units = 50):
+#         """Paper:
+#         The sequence generation is terminated either by the production
+#         of a stop symbol or when the maximum length L has been reached. We implement the decoder
+#         as a single-layer LSTM (p.3)
+#
+#         For the speaker’s message, this is generated in a greedy fashion by
+#         selecting the highest-probability symbol at each step. (p.3)
+#
+#
+#         """
+#         self.speaker_LSTM = tf.keras.Sequential()
+#         #hier brauchen wir glaube ich das Embedding gar nicht da speaker_encoder uns das schon gibt?
+#         self.speaker_LSTM.add(tf.keras.layers.Embedding(input_dim = dim, output_dim = embed_dim))
+#         self.speaker_LSTM.add(tf.keras.LSTM(units = units, return_sequences = True, return_state =True))
+#
+#         pass
+#
+#     def get_speaker_message(self, concept):
+#         self.speaker_LSTM(concept)
+#         pass
+#
+#
+#     #many-to-one LSTM
+#     """
+#
+#     """
+#     def build_listener_LSTM(self,  units = 50, num_words, embed_dim):
+#         """Paper:
+#
+#         """
+#         self.listener_LSTM = tf.keras.Sequential()
+#         self.listener_LSTM.add(tf.keras.layers.Embedding(input_dim = num_words, output_dim = embed_dim) )
+#         self.listener_LSTM.add(tf.keras.LSTM(units)
+#
+#         pass
+#
+#     def get_listeners_message_encoding(self, message):
+#         z = self.listener_LSTM(message)
+#         pass
+#
+#
+#
+#
+#
+#
+#
+#     def calculate_dot_product(self, U, z):
+#         pass
+#
+#     #update die weights aus allen nets
+#     def update(self):
+#         pass
+#
+#
+# #brauchen jeweils eine Funktion um m,u,U,z zu bekommen also,dass es durch das network geht
+#
+# #müssen mit env kommunizieren, ob es ein reward gibt oder nicht
+#
+# #am besten Datentyp anlegen, der u,m,u,U, reward abspeichert pro Durchgang
