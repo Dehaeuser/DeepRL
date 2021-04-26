424c581281cec2137f8e9363577c50ee88ee7821
carokellner <kellner_caroline@web.de>
2021-04-26 19:42:32 +0200

    added sender

---
+++ sender.py
@@ -0,0 +1,99 @@
+import os
+import sys
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Layer, Dense
+from tensorflow.keras.optimizers import Adam
+import tensorflow_probability as tfp
+
+class Encoder(tf.keras.Model):
+    """
+    Encoder class for building speaker's Encoder
+    The encoder receives a concept as binary vector
+    and encodes it into a dense (embedding_dim) representation u
+    """
+    def __init__(self, units=44, categories_dim=597):
+        # TODO: categories_dim anpassen!
+        super(Encoder, self).__init__()
+        self.categories_dim = categories_dim
+        self.input_layer = tf.keras.layers.InputLayer(input_shape=(self.categories_dim))
+        self.act = tf.keras.layers.Dense(units, activation='sigmoid')
+
+    def call(self, input_concept):
+        """
+        input_concept: binary list representing a concept
+        """
+        x = self.input_layer(input_concept)
+        output = self.act(x)
+
+        return output
+
+
+class Sender_LSTM(tf.keras.Model):
+    """
+    LSTM network of the sender that creates
+    the message of length max_m
+    """
+
+    def __init__(self, vocab_size=99, embed_dim, hidden_size, max_len, training=True):
+        super(Sender_LSTM, self).__init__()
+        self.encoder = Encoder()
+        self.vocab_size = vocab_size
+        self.embed_dim = embed_dim
+        self.training = training
+        # set max_len-1 to always replace eos with 0
+        self.max_len = max_len-1
+        self.training = training
+        #self.states = None
+
+        self.output_layer = tf.keras.layers.Dense(units=vocab_size, activation='softmax')
+        #self.lstm = tf.keras.layers.LSTM(units=hidden_size, activation=None, return_sequences=True, return_state=True)
+        self.lstm = tf.keras.layers.RNN(cell=tf.keras.layers.LSTMCell, return_state=True)
+
+    def call(self, input):
+
+        message = []
+        entropy = []
+        logits = []
+        state_h = None
+
+        for i in range(self.max_len):
+            states = None
+            output = None
+            if i == 0:
+                output, state_h, state_c = self.lstm(input)
+                states = [state_h, state_c]
+            else:
+                output, state_h, state_c = self.lstm(input, initial_state=states)
+                states = [state_h, state_c]
+            step_probs = self.output_layer(output)
+            dist = tfp.distributions.Categorical(probs=step_probs)
+
+            if self.training:
+                single_char = dist.sample()
+            else:
+                single_char = tf.math.reduce_max(step_probs, 1)
+            logits.append(dist.log_prob(single_char))
+
+            message.append(single_char)
+
+            entropy.append(dist.entopy())
+
+        return message, logits, entropy, state_h
+        # problem: logits has shape seq_length x output_size
+        # do we sample only from first output logits[0]
+        # logits[0] is output corresponding to target
+
+        # turn lstm output into distribution
+
+        # compute entropy of distribution
+
+        # sample from distribution for every _ in range(self.max_len)
+        # append sample to message
+
+        # embed message to max_len, turn eos to 0
+
+        # maybe loop multiple times (max_len) trough network
+        # save last hidden state and give as input
+
+        # return message, last_state[0], logits, entropy
